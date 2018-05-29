905aa6f9547236a1d6d35aef4e96b312db34b5ec
kavipreethi <kavipreethi1997@gmail.com>
2018-05-29 03:16:13 -0700

    DataTypeExamples

---
+++ src/com/basic/DataTypeExamples.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class DataTypeExamples {
+	public static void main(String[]args)
+	{
+		int count=50;
+		if(count>10)
+		   System.out.println(50);
+		   else
+			   System.out.println(10);
+		
+	}
+
+}
