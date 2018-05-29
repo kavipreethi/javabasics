c504644affffb85d1ebd24c63e1884073ca4c3d7
kavipreethi <kavipreethi1997@gmail.com>
2018-05-29 06:19:21 -0700

    loop

---
+++ src/com/basic/Loop.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class Loop {
+	public static void main(String[]args)
+	{
+		int j= 0;
+		while(j<10)
+		{
+			System.out.println("j=" +j);
+			j++;
+			
+			
+		}
+	}
+
+}
