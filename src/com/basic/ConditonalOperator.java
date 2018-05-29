b0cd2028c8297315a918ffbf0befc67d4f2c7d31
kavipreethi <kavipreethi1997@gmail.com>
2018-05-29 03:50:43 -0700

    SwitchCase

---
+++ src/com/basic/ConditonalOperator.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class ConditonalOperator 
+{
+	public static void main(String[]args)
+	{
+		int count=10;
+		int count < 50 ? 50 : 10;
+		
+	}
+	
+
+}
---
+++ src/com/basic/SwitchCase.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class SwitchCase {
+	public static void main(String[]args)
+	{
+		int chance = 50;
+		switch(chance)
+		{
+		case 10:
+			System.out.println(10);
+			break;
+		case 50:
+			System.out.println(50);
+			break;
+			default:
+				System.out.println("default");
+				
+		}
+	}
+
+}
