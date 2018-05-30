9062b0ccc17e04e79645825dd1badd9af658c536
kavipreethi <kavipreethi1997@gmail.com>
2018-05-29 22:55:42 -0700

    Switch2

--- src/com/basic/ConditonalOperator.java
+++ src/com/basic/ConditonalOperator.java
@@ -4,10 +4,6 @@ public class ConditonalOperator
 {
 	public static void main(String[]args)
 	{
-		int count=10;
-		int count < 50 ? 50 : 10;
 		
-	}
-	
 
 }
---
+++ src/com/basic/switch2.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class switch2 {
+	public static void main(String[]args)
+	{
+	int n = 2;
+	switch (n)
+	{
+	case 1:
+	System.out.println( "The number is 1" );
+	case 2:
+	System.out.println( "The number is 2" );
+	break;
+	default:
+	System.out.println( "The number is not 1 or 2" );
+	break;
+	}
+
+
+	}
+}
