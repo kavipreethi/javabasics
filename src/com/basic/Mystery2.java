cf624af9795b527c493f4b8b6924729cd4b8e522
kavipreethi <kavipreethi1997@gmail.com>
2018-05-30 01:41:24 -0700

    Mystery2

--- src/com/basic/Mystery2.java
+++ src/com/basic/Mystery2.java
@@ -3,17 +3,14 @@ package com.basic;
 public class Mystery2 {
 	public static void main(String[]args)
 	{
-		
 		{
-		int count = 1;
+			int count = 1;
 
-		while ( count <= 10 )
-		{
-		 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
-		 ++count;
-		 } 
-		 } 
-		 } 
-
+			while ( count <= 10 )
+			{
+			 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+			 ++count;
+			}
+		}
+	}
 }
-
