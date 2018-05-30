6445e7a6fcc7b937ad83efbaf43373d3f7703d9b
kavipreethi <kavipreethi1997@gmail.com>
2018-05-29 21:18:39 -0700

    Total

---
+++ src/com/basic/Total.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class Total {
+	public static void main(String[]args)
+	{
+		int x = 1, total = 0;
+		while ( x <= 10 )
+		{
+		total += x;
+		++x;
+		System.out.println(x);
+		}
+
+	}
+
+}
