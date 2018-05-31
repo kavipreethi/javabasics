d3a0f5e20dd691b97bf46a987d4cfc001214b6dd
kavipreethi <kavipreethi1997@gmail.com>
2018-05-30 23:57:22 -0700

    InVoice

--- src/com/basic/Date.java
+++ src/com/basic/Date.java
@@ -5,24 +5,7 @@ public class Date {
 	  int month;
 	int year;
 	
-	public Date(){
-		
-	}
-	public void day() {
-		
-	}
-	public void month() {
 	
-	}
-	public void year() {
-		
-	}
-	public static void main(String[] args)
-	{
-		day 
-		
-		
-	}
 	
 	
 
---
+++ src/com/basic/InVoice.java
@@ -0,0 +1,86 @@
+package com.basic;
+
+public class InVoice {
+	String Number;
+	String Description;
+	int Quantity;
+	double Price;
+	
+	
+
+	public String getNumber() {
+		return Number;
+	}
+
+	public void setNumber(String number) {
+		Number = number;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public int getQuantity() {
+		return Quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		if(Quantity<0)
+		{
+			this.Quantity=0;
+		}
+		else
+		{
+			this.Quantity = Quantity;
+		}
+	}
+		
+		
+
+	public double getPrice() {
+		return Price;
+	}
+
+	public void setPrice(double price) {
+		if(price<0)
+		{
+			this.Price=0.0;
+		}
+		else
+		{
+			this.Price=price;
+		}
+		
+	}
+
+	 public double getInVoiceAmount()
+	{ 
+		
+		return Price*Quantity;
+	}
+	public static void main(String[]args)
+	{
+		InVoice invoice1 = new InVoice();
+		invoice1.setDescription("nut");
+		invoice1.setQuantity(5);
+		invoice1.setPrice(50);
+		System.out.println(invoice1.getQuantity());
+		System.out.println(invoice1.getInVoiceAmount());
+		System.out.println(invoice1.getDescription());
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+
