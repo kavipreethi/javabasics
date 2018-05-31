505b6322762e050c67c2dfd143ef30a7ec4cfff6
kavipreethi <kavipreethi1997@gmail.com>
2018-05-31 05:29:48 -0700

    Method

---
+++ src/com/basic/Method.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class Method {
+	public static void main(String[]args)
+	{
+		int num=2;
+		if( num%2==0 ){
+			
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+	
+
+}
+}
